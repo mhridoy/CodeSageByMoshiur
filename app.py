3f09fdb957b5349f7e307c7dddb095a9ffcf78d5
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2023-12-13 10:51:31 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,146 +1,97 @@
 import streamlit as st
 import pandas as pd
 
-# Page Configuration
-st.set_page_config(page_title='CodeSage By Moshiur', layout='wide', page_icon="🚀")
+# Page configuration
+st.set_page_config(
+    page_title='CodeSage By Moshiur',
+    layout='wide',
+    page_icon='🌈',
+)
 
-# Custom CSS for Styling
+# Custom styles
 st.markdown("""
 <style>
-    /* Global Styles */
-    body {
-        font-family: 'Roboto', sans-serif;
-        background-color: #0E0E10; /* Almost black for a sleek look */
-        color: #E8E6E3; /* Soft white for text */
-    }
-
-    /* Header Style */
-    .header {
-        color: #21A1F1; /* Bright blue for a techy feel */
-        text-align: center;
-        padding: 2rem 0;
-    }
-    .header h1 {
-        font-size: 4rem;
-        font-weight: 700;
-        margin: 0;
-    }
-    .header h1 span {
-        display: block;
-        font-size: 2rem;
-        margin-top: 0.5rem;
-    }
-
-    /* Table Styles */
-    .stTable {
-        box-shadow: 0px 2px 4px rgba(0,0,0,0.1);
-    }
-    .stTable table {
-        border-spacing: 0 15px; /* Space between rows */
-    }
-    .stTable th {
-        background-color: #21A1F1; /* Bright blue header */
-        color: #FFFFFF;
-        font-size: 1.1rem;
-    }
-    .stTable td {
-        background-color: #1C1C1E; /* Darker background for rows */
-        color: #21A1F1; /* Bright blue text for contrast */
-        font-weight: bold;
-    }
-
-    /* YouTube Link Style */
-    .youtube-link {
-        color: #FF0000; /* Bright red for YouTube branding */
-        font-size: 1.5rem;
-        font-weight: bold;
-        text-align: center;
-        padding: 1rem 0;
-    }
-
-    /* Homework Showcase */
-    .homework-section {
-        background-color: #1C1C1E; /* Dark background for emphasis */
+    .main {
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        background-color: #eff3fe;
+        color: #333;
         padding: 2rem;
-        border-radius: 10px;
-        margin-top: 2rem;
-        box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+        border-radius: 0.5rem;
+        margin: 1rem 0;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
-    .homework-section h2 {
-        color: #21A1F1; /* Bright blue */
-        font-size: 2rem;
-    }
-
-    /* Dreamers Academy Mention */
-    .dreamers-mention {
-        background-color: #1C1C1E; /* Darker slate */
-        color: #21A1F1; /* Bright blue text */
-        padding: 1rem;
-        margin: 2rem 0;
-        border-radius: 5px;
-        box-shadow: 0 2px 4px rgba(0,0,0,0.2);
-    }
-
-    /* Footer */
-    .footer {
+    h1 {
+        color: #ff6347; /* Tomato */
         text-align: center;
+    }
+    .schedule-table {
+        background-color: #ffffff;
+        border-radius: 0.5rem;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+    }
+    .youtube-link {
+        color: #ff0000; /* YouTube red */
+        font-weight: bold;
+        text-decoration: none;
+        padding: 0.5rem 1rem;
+        background-color: #fff;
+        border: 2px solid #ff0000;
+        border-radius: 2rem;
+        display: inline-block;
+        margin: 1rem 0;
+    }
+    .homework-section {
+        background-color: #fff;
+        padding: 2rem;
+        margin: 1rem 0;
+        border-radius: 0.5rem;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+    .footer {
+        background-color: #333;
+        color: #fff;
         padding: 1rem;
-        background-color: #111113; /* Slightly lighter black */
-        color: #E8E6E3; /* Soft white */
-        font-size: 0.9rem;
-        margin-top: 5rem;
+        text-align: center;
+    }
+    .dreamers-mention {
+        background-color: #007bff; /* Bootstrap primary blue */
+        color: #fff;
+        padding: 1rem;
+        border-radius: 0.5rem;
+        margin: 1rem 0;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
+        text-align: center;
     }
 </style>
 """, unsafe_allow_html=True)
 
-# Header with Title and Subtitle
-st.markdown("""
-<div class="header">
-    <h1>CodeSage By Moshiur <span>Empowering the Next Generation of Coders</span></h1>
-</div>
-""", unsafe_allow_html=True)
+# Main title
+st.markdown('<div class="main"><h1>CodeSage By Moshiur</h1></div>', unsafe_allow_html=True)
 
-# YouTube Link Section
-youtube_url = "https://www.youtube.com/c/YourChannel"  # Replace with your YouTube channel link
-st.markdown(f"""
-<p class="youtube-link">
-    Check out my <a href="{youtube_url}" target="_blank">YouTube channel</a> for more tutorials!
-</p>
-""", unsafe_allow_html=True)
-
-# Class Schedule with Google Colab Links
-st.markdown('## 📅 Class Schedule')
-
-# Schedule Data with Google Colab Links (example links used here)
+# Schedule Table
+st.markdown('## Class Schedule')
 schedule_data = {
     'Batch Number': [f'Batch {i+1}' for i in range(13)],
-    'Schedule': ["TBD" for _ in range(13)],  # Replace 'TBD' with actual schedules
-    'Google Colab Link': [f'[Open Colab](https://colab.research.google.com/batch{i+1})' for i in range(13)]
+    'Schedule': ['Monday & Wednesday: 4pm - 5pm' for _ in range(13)],
+    'Google Colab Link': [f'[Open in Colab](https://colab.research.google.com/batch{i+1})' for i in range(13)],
 }
-
-# DataFrame for Schedule
 schedule_df = pd.DataFrame(schedule_data)
-st.table(schedule_df)
+st.table(schedule_df.style.set_properties(**{'background-color': '#FFFFFF', 'color': '#333'}))
 
-# Best Homework of the Month Section
-st.markdown("""
-<div class="homework-section">
-    <h2>🌟 Best Homework of the Month 🌟</h2>
-    <!-- Replace 'path_to_homework_image' with the actual path to the homework image -->
-    <img src="path_to_homework_image" alt="Best Homework" style="width:100%; border-radius:10px;">
-</div>
-""", unsafe_allow_html=True)
+# YouTube Section
+youtube_url = "https://www.youtube.com/c/YourChannelName"  # Replace with your YouTube channel link
+st.markdown(f'<a href="{youtube_url}" target="_blank" class="youtube-link">Visit my YouTube Channel</a>', unsafe_allow_html=True)
+
+# Best Homework Section
+st.markdown('## Best Homework of the Month')
+st.image("path_to_homework_image.jpg", caption='Great job on this project!', use_column_width=True)  # Replace with the path to your image
 
 # Dreamers Academy Mention
 st.markdown("""
 <div class="dreamers-mention">
-    Proudly teaching Python at <a href="https://dreamersacademy.com.bd/" target="_blank">Dreamers Academy</a>, where we nurture young minds in the world of programming.
+    <p>I'm a proud instructor at <a href="https://dreamersacademy.com.bd/" target="_blank" style="color: #fff;">Dreamers Academy</a>, where we teach kids the joy of coding through Python!</p>
 </div>
 """, unsafe_allow_html=True)
 
 # Footer
-st.markdown("""
-<div class="footer">
-    © 2023 CodeSage By Moshiur. All Rights Reserved. In collaboration with Dreamers Academy.
-</div>
-""", unsafe_allow_html=True)
+st.markdown('<div class="footer">© 2023 CodeSage By Moshiur. All Rights Reserved.</div>', unsafe_allow_html=True)
