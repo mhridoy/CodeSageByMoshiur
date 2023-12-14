041b8962211f65ddbea7bcd1389694276f7af011
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2023-12-14 06:49:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ st.set_page_config(
     page_icon='🌟'
 )
 
-# Define a harmonious color palette
+# Define an advanced, soothing color scheme
 colors = {
-    'background': '#FAFAFA',  # A gentle off-white background
-    'primary': '#005f73',     # A calming dark teal
-    'secondary': '#0a9396',   # A soothing teal
-    'accent': '#94d2bd',      # A soft green accent
-    'text': '#495057',        # A dark gray for text for readability
-    'button': '#ee9b00',      # A warm amber for buttons and highlights
-    'button_hover': '#ca6702',  # A darker shade for button hover state
+    'background': '#F7F7FF',  # A very light gray that's easy on the eyes
+    'primary': '#6246EA',     # A rich, calming purple
+    'secondary': '#E45858',   # A soft, warm red for contrasts
+    'accent': '#D1D1E9',      # A muted lavender for accents and highlights
+    'text': '#2B2C34',        # A deep, dark purple for text
+    'button_bg': '#FFD803',   # A cheerful yellow for buttons
+    'button_hover': '#FFC30B',  # A golden hue for button hover state
 }
 
 # Custom styles
@@ -24,33 +24,28 @@ st.markdown(f"""
 <style>
     /* Global Styles */
     body {{
-        font-family: 'Calibri', sans-serif;
+        font-family: 'Montserrat', sans-serif;
         background-color: {colors['background']};
         color: {colors['text']};
     }}
     
-    h1 {{
+    h1, h2, h3, h4, h5, h6 {{
         color: {colors['primary']};
-        font-weight: bold;
-    }}
-    
-    h2 {{
-        color: {colors['secondary']};
     }}
     
     .stButton > button {{
-        border: 2px solid transparent;
+        border: none;
         border-radius: 30px;
-        background-color: {colors['button']};
+        background-color: {colors['button_bg']};
         color: {colors['background']};
         padding: 0.5rem 1rem;
-        transition: background-color 0.3s, border-color 0.3s;
+        margin: 0.5rem 0;
+        transition: background-color 0.3s, transform 0.3s;
     }}
     
     .stButton > button:hover {{
-        background-color: {colors['background']};
-        border-color: {colors['button_hover']};
-        color: {colors['button']};
+        background-color: {colors['button_hover']};
+        transform: translateY(-3px);
     }}
     
     .stDataFrame, .stTable {{
@@ -67,8 +62,8 @@ st.markdown(f"""
     }}
     
     .dataframe td {{
-        background-color: {colors['secondary']};
-        color: {colors['background']};
+        background-color: {colors['background']};
+        color: {colors['text']};
     }}
     
     .footer {{
@@ -88,16 +83,21 @@ st.markdown(f"""
         border-radius: 0.5rem;
         margin: 1rem 0;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease-in-out;
+    }}
+    
+    .custom-section:hover {{
+        transform: scale(1.02);
     }}
     
     a {{
-        color: {colors['button']};
+        color: {colors['secondary']};
         text-decoration: none;
         transition: color 0.2s;
     }}
     
     a:hover {{
-        color: {colors['button_hover']};
+        color: {colors['primary']};
         text-decoration: underline;
     }}
     
@@ -110,7 +110,7 @@ st.markdown(f'<h1>CodeSage By Moshiur</h1>', unsafe_allow_html=True)
 # Main content area
 col1, col2 = st.columns(2)
 with col1:
-    st.markdown('## Class Schedule')
+    st.markdown('## Class Schedule 📚')
     # Schedule Data
     schedule_data = {
         'Batch': [f'Batch {i+1}' for i in range(6)],
@@ -119,31 +119,35 @@ with col1:
     }
     # DataFrame for Schedule
     schedule_df = pd.DataFrame(schedule_data)
-    st.dataframe(schedule_df)
+    st.dataframe(schedule_df.style.set_properties(**{
+        'background-color': colors['background'],
+        'color': colors['text']
+    }))
 
 with col2:
-    st.markdown('## Best Homework of the Month')
-    st.image('https://wallpapers.com/images/hd/coding-background-9izlympnd0ovmpli.jpg', caption='Incredible work by our star coder!', use_column_width=True)
+    st.markdown('## Best Homework of the Month 🌟')
+    st.image('path_to_homework_image.jpg', caption='Incredible work by our star coder!', use_column_width=True)
 
 # Dreamers Academy Mention
 st.markdown(f"""
 <div class="custom-section">
-    <h2>Dreamers Academy Collaboration</h2>
-    <p>As an instructor at <a href="https://dreamersacademy.com.bd/" target="_blank">Dreamers Academy</a>, I take pride in guiding students through their Python programming journey. Join us to explore the world of coding!</p>
+    <h2>Dreamers Academy Collaboration 🎓</h2>
+    <p>Join us at <a href="https://dreamersacademy.com.bd/" target="_blank">Dreamers Academy</a> and start your journey with Python programming. It's a perfect place to turn curiosity into creativity!</p>
 </div>
 """, unsafe_allow_html=True)
 
 # YouTube Channel Link
+youtube_url = "https://www.youtube.com/c/YourChannelName"  # Replace with your YouTube channel link
 st.markdown(f"""
 <div class="custom-section">
-    <h2>Visit our YouTube Channel</h2>
-    <p>Discover more learning resources and tutorials on our <a href="https://www.youtube.com/c/YourChannelName" target="_blank">YouTube channel</a>.</p>
+    <h2>Explore our YouTube Channel 🎥</h2>
+    <p>Check out our <a href="{youtube_url}" target="_blank">YouTube channel</a> for engaging tutorials and learning resources.</p>
 </div>
 """, unsafe_allow_html=True)
 
 # Footer
-st.markdown("""
+st.markdown(f"""
 <div class="footer">
-    &copy; 2023 CodeSage By Moshiur. All Rights Reserved.
+    © 2023 CodeSage By Moshiur. All Rights Reserved.
 </div>
 """, unsafe_allow_html=True)
