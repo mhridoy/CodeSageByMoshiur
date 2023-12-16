568568b6c370f08d5cb27d00501cab42ef286777
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2023-12-16 06:40:35 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,221 @@
+import streamlit as st
+import pandas as pd
+import sys
+import io
+
+# Function to load schedule data from the Excel file
+def load_schedule():
+    file_path = 'schedule.xlsx'  # Ensure this path is correct
+    return pd.read_excel(file_path)
+
+# Page configuration
+st.set_page_config(
+    page_title='CodeSage By Moshiur',
+    layout='wide',
+    page_icon='🦋',
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
+
+# Define a creative and soothing light purple color scheme
+colors = {
+    'background': '#FAF4FF',  # A very light purple for a serene background
+    'primary': '#7B2CBF',     # A deep purple for contrast
+    'secondary': '#9D4EDD',   # A softer purple for secondary elements
+    'accent': '#CDB4DB',      # A gentle lavender for highlights
+    'text': '#4A2040',        # A darker purple for text
+    'footer_bg': '#7B2CBF',   # Deep purple for the footer
+    'footer_text': '#EDE9F4'  # Light purple text for the footer
+}
+
+# Custom styles
+st.markdown(f"""
+<style>
+    /* Global Styles */
+    body {{
+        font-family: 'Segoe UI', sans-serif;
+        background-color: {colors['background']};
+        color: {colors['text']};
+    }}
+
+    h1 {{
+        color: {colors['primary']};
+        font-size: 2.5em;
+        text-align: center;
+    }}
+
+    h2 {{
+        color: {colors['secondary']};
+        font-size: 1.75em;
+    }}
+
+    /* Custom Section Style */
+    .custom-section {{
+        background-color: {colors['accent']};
+        padding: 2em;
+        border-radius: 15px;
+        margin-bottom: 1em;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease;
+    }}
+
+    .custom-section:hover {{
+        transform: scale(1.03);
+    }}
+
+    /* Course Section Style */
+    .course-section {{
+        border-left: 5px solid {colors['secondary']};
+        background-color: {colors['background']};
+        padding: 1em;
+        margin-bottom: 1em;
+    }}
+
+    .course-title {{
+        color: {colors['primary']};
+        font-weight: bold;
+    }}
+
+    .course-description {{
+        color: {colors['text']};
+    }}
+
+    /* Link Style */
+    a {{
+        color: {colors['secondary']};
+        text-decoration: none;
+    }}
+
+    a:hover {{
+        color: {colors['primary']};
+        text-decoration: underline;
+    }}
+
+    /* Footer Style */
+    .footer {{
+        background-color: {colors['footer_bg']};
+        color: {colors['footer_text']};
+        padding: 1em;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+    }}
+
+    /* Table Style */
+    .stDataFrame, .stTable {{
+        border-radius: 8px;
+        overflow: hidden;
+    }}
+
+    .dataframe th {{
+        background-color: {colors['secondary']};
+        color: {colors['text']};
+    }}
+
+    .dataframe td {{
+        background-color: {colors['background']};
+        color: {colors['text']};
+    }}
+
+    /* Button Style */
+    .stButton > button {{
+        border: 2px solid {colors['secondary']};
+        border-radius: 20px;
+        background-color: {colors['accent']};
+        color: {colors['text']};
+        padding: 0.5rem 1rem;
+        transition: all 0.3s;
+    }}
+
+    .stButton > button:hover {{
+        background-color: {colors['primary']};
+        color: {colors['background']};
+    }}
+
+    /* Hiding Streamlit elements */
+    .css-1y0tads, .css-1v3fvcr, .css-1r6o8ze {{
+        visibility: hidden;
+    }}
+    footer {{
+        visibility: hidden;
+    }}
+    .block-container {{
+        padding-bottom: 5rem;
+    }}
+</style>
+""", unsafe_allow_html=True)
+
+# Header
+st.markdown(f'<h1> Dreamers Academy - Track 3 | CodeSage By Moshiur</h1>', unsafe_allow_html=True)
+
+# Load the schedule data
+schedule_df = load_schedule()
+
+# Main content area
+col1, col2 = st.columns([3, 2])
+with col1:
+    st.markdown('<h2>Class Schedule 📚</h2>', unsafe_allow_html=True)
+    st.dataframe(schedule_df.style.set_properties(**{
+        'background-color': colors['background'],
+        'color': colors['text']
+    }))
+
+with col2:
+    st.markdown('<h2>Best Homework of the Month 🌟 Sababah Subah</h2>', unsafe_allow_html=True)
+    st.image('best_homework.png', caption='Incredible work by our star coder!', use_column_width=True)
+
+# Interactive Python Editor
+st.markdown("## Interactive Python Editor")
+code = st.text_area("Write your Python code here", height=250)
+if st.button('Run Code'):
+    # Redirect output
+    old_stdout = sys.stdout
+    new_stdout = io.StringIO()
+    sys.stdout = new_stdout
+
+    try:
+        # Execute code
+        exec(code)
+    except Exception as e:
+        st.error(e)
+    finally:
+        # Display output
+        sys.stdout = old_stdout
+        st.text(new_stdout.getvalue())
+
+# Display courses 
+for course in courses:
+    st.markdown(f"""
+    <div class="course-section">
+        <h2 class="course-title">{course['title']}</h2>
+        <p class="course-description">{course['description']}</p>
+    </div>
+    """, unsafe_allow_html=True)
+
+# Dreamers Academy Mention
+st.markdown(f"""
+<div class="custom-section">
+    <h2>Dreamers Academy Collaboration 🎓</h2>
+    <p>Join us at <a href="https://dreamersacademy.com.bd/" target="_blank">Dreamers Academy</a> and start your journey with Python programming. It's a perfect place to turn curiosity into creativity!</p>
+</div>
+""", unsafe_allow_html=True)
+
+# YouTube Channel Link
+youtube_url = "https://youtube.com/mhridoy"
+st.markdown(f"""
+<div class="custom-section">
+    <h2>Explore our YouTube Channel 🎥</h2>
+    <p>Check out our <a href="{youtube_url}" target="_blank">YouTube channel</a> for engaging tutorials and learning resources.</p>
+</div>
+""", unsafe_allow_html=True)
+
+# Footer
+st.markdown(f"""
+<div class="footer">
+    © 2023 CodeSage By Moshiur. All Rights Reserved.
+</div>
+""", unsafe_allow_html=True)
