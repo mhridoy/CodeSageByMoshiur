890849525b184f48488339628a0e7265ea851257
Md.Moshiur Rahman <35825505+mhridoy@users.noreply.github.com>
2023-12-17 14:27:23 +0600

    Update script.js

--- code_editor/script.js
+++ code_editor/script.js
@@ -1,4 +1,13 @@
 // Initialize Ace Editor
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/github"); // Light theme
-editor.session.setMode("ace/mode/python"); // Set language mode
+editor.setTheme("ace/theme/github");
+editor.session.setMode("ace/mode/python");
+
+document.getElementById("run-btn").addEventListener("click", function() {
+    // Fetch code from the editor
+    var code = editor.getValue();
+
+    // Mock code execution and display output
+    // Replace this part with actual code execution logic
+    document.getElementById("output").innerText = "Executing Python code:\n" + code;
+});
